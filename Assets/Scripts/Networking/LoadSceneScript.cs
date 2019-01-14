1a95da0296410e331140615c6090869a2ec5b2c1
leom <leom@itu.dk>
2019-01-14 16:01:17 +0100

    gtr

---
+++ Assets/Scripts/Networking/LoadSceneScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneScript : MonoBehaviour {
+
+	public void LoadScene(int sceneToLoad)
+	{
+		SceneManager.LoadScene(sceneToLoad);
+	}
+}
