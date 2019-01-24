5391fd4a34ce58052aa5da17c62159e069f3fdd0
leom <leom@itu.dk>
2019-01-24 11:04:04 +0100

    sdfgsd

--- Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
+++ Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -29,7 +29,6 @@ public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandle
 
     protected virtual void Start()
     {
-        TrackerManager.Instance.GetStateManager().ReassociateTrackables();
         mTrackableBehaviour = GetComponent<TrackableBehaviour>();
         if (mTrackableBehaviour)
             mTrackableBehaviour.RegisterTrackableEventHandler(this);
