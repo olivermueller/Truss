053e748fabf3927aba7bac91e578955f1ce9ca48
leom <leom@itu.dk>
2019-01-24 14:48:28 +0100

    bkjjbkjb

--- Assets/Scripts/Networking/PlayerUnit.cs
+++ Assets/Scripts/Networking/PlayerUnit.cs
@@ -99,7 +99,7 @@ public class PlayerUnit : NetworkBehaviour
         
 //        GameStateManager.CmdSetNodeId((FindObjectOfType<TestingScript>().iterator as AnswerTaskData).noTask.ID);
         GameStateManager._testingScript= FindObjectOfType<TestingScript>();
-        //GameStateManager._testingScript.iterator = (GameStateManager._testingScript.iterator as AnswerTaskData).noTask;
+        GameStateManager._testingScript.iterator = (GameStateManager._testingScript.iterator as AnswerTaskData).noTask;
         print("iterator ID: " + GameStateManager._testingScript.iterator.ID);
         GameStateManager.CmdSetNodeId(GameStateManager._testingScript.iterator.ID);
         
