1235def30a32e537a40c012d684183089ee68d40
leom <leom@itu.dk>
2019-03-20 12:23:37 +0100

    dsasd

---
+++ Assets/Scripts/ActivateFlashlight.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ActivateFlashlight : MonoBehaviour
+{
+	private bool active = false;
+	public void SwitchFlashlight()
+	{
+		active = !active;
+		CameraDevice.Instance.SetFlashTorchMode( active );
+	}
+}
